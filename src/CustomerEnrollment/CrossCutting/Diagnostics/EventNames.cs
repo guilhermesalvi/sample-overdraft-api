60d180560d430c67f771a143b65207c3a1511b25
Guilherme Salvi <guisalvi@outlook.com>
2025-09-14 17:02:03 -0300

    refactor: add activity extension

--- src/CustomerEnrollment/CrossCutting/Diagnostics/EventNames.cs
+++ src/CustomerEnrollment/CrossCutting/Diagnostics/EventNames.cs
@@ -3,10 +3,10 @@
 public static class EventNames
 {
     public const string HandlerStart = "handler.start";
-    public const string HandlerEnd   = "handler.end";
-    public const string QueryStart   = "query.start";
-    public const string QueryEnd     = "query.end";
-    public const string Error        = "error";
+    public const string HandlerEnd = "handler.end";
+    public const string QueryStart = "query.start";
+    public const string QueryEnd = "query.end";
+    public const string Error = "error";
     public const string ValidationStart = "validation.start";
-    public const string ValidationEnd   = "validation.end";
+    public const string ValidationEnd = "validation.end";
 }
